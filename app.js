057b1ef97777548c90b549fe68415b05d62c6eb9
Crypto Gnome <33667144+CryptoGnome@users.noreply.github.com>
2023-03-08 10:24:45 -0500

    Favor Fix for Battle

--- app.js
+++ app.js
@@ -324,6 +324,12 @@ async function battle(username, _target, memo) {
             roll = Math.floor(Math.random() * 50) + 1;
         }
 
+        //if attacker has zero favor
+        if (user.favor == 0) {
+            //roll a number between 1 and 25
+            roll = Math.floor(Math.random() * 25) + 1;
+        }
+
         //allow user to take target scrap up to the amount of damage left after target defense and add it to user damage
         var scrapToSteal = user.damage - target.defense;
 
